bcd4447139410c9f1cbcb277897a58d20535af6c
Lilia <trek0952056799@gmail.com>
2022-12-18 17:43:29 +0100

    created super class

--- src/main/java/org/lilia/models/BaseClass.java
+++ src/main/java/org/lilia/models/BaseClass.java
@@ -1,7 +1,30 @@
 package org.lilia.models;
 
 public class BaseClass {
-    //private String name;
+    private static int counter = 0;
 
+    private final int id;
+    private String name;
 
+    public static int getCounter() {
+        return counter;
+    }
+
+    public static void setCounter(int counter) {
+        BaseClass.counter = counter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BaseClass(String name) {
+        this.name = name;
+        counter++;
+        id = counter;
+    }
 }
